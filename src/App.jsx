6fc60ec2ae6dd05c2dd88839179108c5103be2dc
مصطفى خالد <atyaty003@gmal.com>
2025-10-14 21:04:46 +0300

    first push

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
       <NavBar></NavBar>
 
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route index path="/" element={<Home />} />
         <Route path="/about" element={<h1>About</h1>} />
         <Route path="/contact" element={<h1>Contact</h1>} />
         <Route path="/blog" element={<h1>Blog</h1>} />
